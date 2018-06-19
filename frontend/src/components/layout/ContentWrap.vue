e539db6feb447d3c6f58630474ed762742581973
betterfly <betterfly4202@gmail.com>
2018-06-20 01:07:31 +0900

    no message

---
+++ frontend/src/components/layout/ContentWrap.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="page-wrapper">
+    <router-view></router-view>
+    <!-- <footer class="footer text-center"> 2017 &copy; Ample Admin brought to you by themedesigner.in </footer> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentWrap',
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+.page-enter-active, .page-leave-active {
+  transition: opacity 0.5s, transform 0.5s;
+}
+.page-enter, .page-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ frontend/src/pages/mail/MailService.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <div class="row">
+    <div class="col-lg-12">
+      <h1 class="page-header">일일 업무 보고</h1>
+
+      <div class="form-group">
+        <a class="btn btn-success" type="button" onclick="mailSend()">
+          보내기
+        </a>
+      </div>
+
+      <div class="panel-body">
+        <div class="row">
+          <div class="col-lg-6">
+
+            <div class="form-group input-group">
+              <input type="text" class="form-control" placeholder="수신자" id="mailReceiver">
+              <span class="input-group-btn">
+                                    <a class="btn btn-default" type="button" onclick="onRefReceiver()">참조
+                                    </a>
+                                </span>
+            </div>
+
+            <div class="form-group" id="ref-receiver" hidden>
+              <input class="form-control" placeholder="참조">
+            </div>
+
+            <div class="form-group">
+              <input class="form-control" placeholder="제목" id="mailSubject">
+            </div>
+
+            <div class="form-group" style="margin-top: 10px;">
+              <input type="file">
+            </div>
+
+            <textarea name="editor" id="editor">
+            </textarea>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.col-lg-12 -->
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'MailService',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
