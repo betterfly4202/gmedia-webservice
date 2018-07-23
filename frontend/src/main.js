a8cfae378e598e331643b88876a5e2e4d822ab07
betterfly <betterfly4202@gmail.com>
2018-07-24 00:02:37 +0900

    no message

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+require('es6-promise').polyfill();
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+axios.defaults.headers.common['AJAX'] = 'true';
+
+// $http.interceptors.response.use(null, function (error) {
+//   console.error(error)
+//   if (error.status === 401) {
+//     console.error('error 401')
+//   }
+//   return Promise.reject(error)
+// })
 
 /* eslint-disable no-new */
 new Vue({
