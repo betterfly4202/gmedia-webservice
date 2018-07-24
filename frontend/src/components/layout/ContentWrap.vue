709b6e78d7b6fd32309ead89acf92e3158cc4e96
betterfly <betterfly4202@gmail.com>
2018-07-25 00:03:01 +0900

    no message

--- frontend/src/components/layout/ContentWrap.vue
+++ frontend/src/components/layout/ContentWrap.vue
@@ -1,23 +1,108 @@
 <template>
-  <div id="page-wrapper">
-    <router-view></router-view>
-    <!-- <footer class="footer text-center"> 2017 &copy; Ample Admin brought to you by themedesigner.in </footer> -->
+  <div id="wrapper">
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Gmedia Web Service</a>
+      </div>
+      <!-- /.navbar-header -->
+
+      <ul class="nav navbar-top-links navbar-right">
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+            <i class="fa fa-envelope fa-fw"></i> <i class="fa fa-caret-down"></i>
+          </a>
+          <ul class="dropdown-menu dropdown-messages">
+            <li>
+              <a href="#">
+                <div>
+                  <strong>John Smith</strong>
+                  <span class="pull-right text-muted">
+                                        <em>Yesterday</em>
+                                    </span>
+                </div>
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
+              </a>
+            </li>
+            <li class="divider"></li>
+            <li>
+              <a href="#">
+                <div>
+                  <strong>John Smith</strong>
+                  <span class="pull-right text-muted">
+                                        <em>Yesterday</em>
+                                    </span>
+                </div>
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
+              </a>
+            </li>
+          </ul>
+          <!-- /.dropdown-messages -->
+        </li>
+        <!-- /.dropdown -->
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+            <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+          </a>
+          <ul class="dropdown-menu dropdown-user">
+            <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+            </li>
+            <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+            </li>
+            <li class="divider"></li>
+            <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+            </li>
+          </ul>
+          <!-- /.dropdown-user -->
+        </li>
+        <!-- /.dropdown -->
+      </ul>
+      <!-- /.navbar-top-links -->
+
+      <div class="navbar-default sidebar" role="navigation">
+        <div class="sidebar-nav navbar-collapse">
+          <ul class="nav" id="side-menu">
+            <li>
+              <a href="#"><i class="fa fa-envelope-o fa-fw"></i> Send Mail</a>
+              <ul class="nav nav-second-level">
+                <li>
+                  <a href="notifications.html">받은 메일함</a>
+                </li>
+                <li>
+                  <a href="buttons.html">보낸 메일함</a>
+                </li>
+                <li>
+                  <a href="panels-wells.html">임시저장</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <!-- /.sidebar-collapse -->
+      </div>
+      <!-- /.navbar-static-side -->
+    </nav>
+    <MailService></MailService>
+
   </div>
 </template>
 
 <script>
+import MailService from '../../pages/mail/MailService'
+
 export default {
   name: 'ContentWrap',
+  components: {
+    'MailService': MailService
+  },
+
   created () {
   }
 }
 </script>
-
-<style scoped>
-.page-enter-active, .page-leave-active {
-  transition: opacity 0.5s, transform 0.5s;
-}
-.page-enter, .page-leave-to {
-  opacity: 0;
-}
-</style>
--- frontend/src/pages/mail/MailService.vue
+++ frontend/src/pages/mail/MailService.vue
@@ -1,59 +1,84 @@
 <template>
+  <div id="page-wrapper">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1 class="page-header">일일 업무 보고_vue</h1>
 
-  <div class="row">
-    <div class="col-lg-12">
-      <h1 class="page-header">일일 업무 보고_vue</h1>
+        <div class="form-group">
+          <a class="btn btn-success" type="button" @click="sendMail()">
+            보내기
+          </a>
+        </div>
 
-      <div class="form-group">
-        <a class="btn btn-success" type="button" onclick="mailSend()">
-          보내기
-        </a>
-      </div>
+        <div class="panel-body">
+          <div class="row">
+            <div class="col-lg-6">
 
-      <div class="panel-body">
-        <div class="row">
-          <div class="col-lg-6">
+              <div class="form-group input-group">
+                <input type="text" class="form-control" placeholder="수신자" v-model="receiver">
+                <span class="input-group-btn">
+                                      <a class="btn btn-default" type="button" onclick="onRefReceiver()">참조
+                                      </a>
+                                  </span>
+              </div>
 
-            <div class="form-group input-group">
-              <input type="text" class="form-control" placeholder="수신자" id="mailReceiver">
-              <span class="input-group-btn">
-                                    <a class="btn btn-default" type="button" onclick="onRefReceiver()">참조
-                                    </a>
-                                </span>
+              <div class="form-group" id="ref-receiver" hidden>
+                <input class="form-control" placeholder="참조">
+              </div>
+
+              <div class="form-group">
+                <input class="form-control" placeholder="제목" v-model="subject">
+              </div>
+
+              <div class="form-group" style="margin-top: 10px;">
+                <input type="file">
+              </div>
+
+              <VueEditor v-model="contents"></VueEditor>
+
             </div>
-
-            <div class="form-group" id="ref-receiver" hidden>
-              <input class="form-control" placeholder="참조">
-            </div>
-
-            <div class="form-group">
-              <input class="form-control" placeholder="제목" id="mailSubject">
-            </div>
-
-            <div class="form-group" style="margin-top: 10px;">
-              <input type="file">
-            </div>
-
-            <textarea name="editor" id="editor">
-            </textarea>
           </div>
         </div>
       </div>
+      <!-- /.col-lg-12 -->
     </div>
-    <!-- /.col-lg-12 -->
   </div>
 
 </template>
 
 <script>
-export default {
-  name: 'MailService',
-  data () {
-    return {
+  import axios from "axios";
+  import VueEditor from 'vue2-editor'
 
+  export default {
+    name: 'MailService',
+    components: VueEditor,
+
+    data () {
+      return {
+        subject:'zvsvasdfsf',
+        receiver:'dlcnddlf88@naver.com',
+        contents:'send mail'
+      }
+    },
+
+    methods: {
+      sendMail(){
+        const url = 'http://localhost:8080/sender';
+//        var url = Utils.makePathVariableURL('monimanager', 'web', 'api', 'atm-status', 'remove');
+        axios.post(url,{
+          subject : this.subject,
+          internetAddress : this.receiver.split(";"),
+          contents : this.contents
+        }).then(response => {
+          console.log('send success');
+        }).catch(error => {
+          console.log(error);
+        });
+      },
     }
+
   }
-}
 </script>
 
 <style scoped>
